16c877246506912547fd37c56a136313313ce6ac
Irina Shchedrina <irina.shchedrina@snowflake.com>
2024-02-12 19:41:46 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-import serialization
+from cryptography.hazmat.primitives import serialization
 
 """
 # Welcome to Streamlit!
